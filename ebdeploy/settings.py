658737b9f85eb22ca3ce8924c32df14d239f0a39
Suhun <soohon@naver.com>
2020-05-19 19:31:48 +0900

    와우

--- ebdeploy/settings.py
+++ ebdeploy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ebdeploy.app.EbdeployConfig',
 ]
 
 MIDDLEWARE = [
---
+++ ebdeployapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EbdeployappConfig(AppConfig):
+    name = 'ebdeployapp'
